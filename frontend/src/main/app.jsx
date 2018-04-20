f066da1f6ac826959fcb48c18728955ed10f5283
Edmundo Nascimento <edmundonascimento@MacBook-Pro-de-Edmundo.local>
2018-04-20 01:03:06 -0300

    criacao do backend e do front( header, sidebar, menu and menuItem)

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../commom/template/dependencies'
+
+import Header from '../commom/template/header'
+import SideBar from '../commom/template/sidebar'
+
+export default props =>(
+    <div className="wrapper">
+        <Header />
+        <SideBar />
+    </div>
+)

dd78c49cf9c80d33d0a4a3a12f0f45cc1f19a61c
Edmundo Nascimento <edmundonascimento@MacBook-Pro-de-Edmundo.local>
2018-04-20 08:23:06 -0300

    create components Dashboard and BillingCycle - create to Routes of app

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,13 +4,14 @@ import '../commom/template/dependencies'
 import Header from '../commom/template/header'
 import SideBar from '../commom/template/sidebar'
 import Footer from '../commom/template/footer'
+import Routes from './routes'
 
 export default props =>(
     <div className="wrapper">
         <Header />
         <SideBar />
         <div className="content-wrapper">
-            <h1>Conteudo</h1>
+            <Routes /> 
         </div>
         <Footer />
     </div>

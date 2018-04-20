6fd504a10cc9324ecb434683b35b83718a193915
Edmundo Nascimento <edmundonascimento@MacBook-Pro-de-Edmundo.local>
2018-04-20 08:05:14 -0300

    create footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,15 @@ import '../commom/template/dependencies'
 
 import Header from '../commom/template/header'
 import SideBar from '../commom/template/sidebar'
+import Footer from '../commom/template/footer'
 
 export default props =>(
     <div className="wrapper">
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteudo</h1>
+        </div>
+        <Footer />
     </div>
 )

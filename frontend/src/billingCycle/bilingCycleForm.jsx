f877a81dc3d0f2fc52e090d8e97fe7a1b9fc04a0
Edmundo Nascimento <edmundonascimento@MacBook-Pro-de-Edmundo.local>
2018-05-02 08:48:20 -0300

    conf do form

--- frontend/src/billingCycle/bilingCycleForm.jsx
+++ frontend/src/billingCycle/bilingCycleForm.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 
+import labelAndInput from '../commom/form/labelAndInput'
+
 class BillingCycleForm extends Component {
     render() {
         const { handleSubmit } = this.props
-        console.log(handleSubmit)
         return (
             <form role="form" onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -16,7 +17,7 @@ class BillingCycleForm extends Component {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </div>
             </form>
-        );
+        )
     }
 }
 export default reduxForm({form: 'billingCycleForm'})(BillingCycleForm)

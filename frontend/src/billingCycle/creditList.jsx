681fcb8d48513e4ffb59485cc8047333700c28bb
Edmundo Nascimento <edmundonascimento@MacBook-Pro-de-Edmundo.local>
2018-05-05 18:42:05 -0300

    conf do btn remove da aba incluir

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Field, arrayInsert } from 'redux-form'
+import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
@@ -11,6 +11,13 @@ class CreditList extends Component {
     add(index, item = {}) {
         if(!this.props.readOnly){
             this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
+
+    remove(index) {
+        //remove se os elementos do array forem > 1 e se o campo n for somente leitura
+        if(!this.props.readOnly && this.props.list.length > 1){
+            this.props.arrayRemove('billingCycleForm', 'credits', index)
         }
     }
 
@@ -27,6 +34,8 @@ class CreditList extends Component {
                         onClick={() => this.add(index + 1)}><i className="fa fa-plus"></i></button>
                     <button type="button" className="btn btn-warning"
                         onClick={() => this.add(index + 1, item)}><i className="fa fa-clone"></i></button>
+                    <button type="button" className="btn btn-danger"
+                        onClick={() => this.remove(index)}><i className="fa fa-trash-o"></i></button>
                 </td>
             </tr>
         ))
@@ -56,6 +65,14 @@ class CreditList extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({arrayInsert, arrayRemove}, dispatch)
 
 export default connect(null, mapDispatchToProps)(CreditList)
+
+
+
+/*
+    import { Field, arrayInsert, arrayRemove } from 'redux-form' - este treclho é que faz com que os campos inputs sejam:
+    add, clonados e removidos
+
+*/

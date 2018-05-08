780188c67c055943bff77ac93c8c02025173a16b
Edmundo Nascimento <edmundonascimento@MacBook-Pro-de-Edmundo.local>
2018-05-08 11:27:20 -0300

    autenticacao front end - authService more alteracoes

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -16,6 +16,39 @@ const sendErrorsFromDB = (res, dbErrors) => {
     })
 }
 
+const login = (req, res, next) => {
+    //a partir do request eu vou pegar o email e password que forem enviados
+    const email = req.body.email || '' 
+    const password = req.body.password || ''
+
+    //dentro da colecao de usuários (User), eu procuro apenas um user(findOne), e faco um filtro pelo email
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env.authSecret, {
+                expiresIn: "1 day"
+            })
+            const { name, email } = user
+            res.json({ name, email, token })
+        } else {
+            return res.status(400).send({ errors: ['Usuário/Senha inválidos'] }) //msg de senha ou user invélidos
+        }
+    })
+}
+
 /*
     responsavel pelos services da autenticacao (registrar, novo user, signup, login e validar o token)
+
+    1 - if (user && bcrypt.compareSync(password, user.password)) {
+    2 -     const token = jwt.sign(user, env.authSecret, {
+    3 -        expiresIn: "1 day"
+    4 -    })
+    5 -    const { name, email } = user //extreio o name e o email de user
+    6 -    res.json({ name, email, token }) // gero um objeto com o nome, email e o token e transformo em json
+        }
+
+    1 - se o user for true, tb vou comparar sincronamente a senha que foi digitada com a senha que obtive do user user.password
+    2 - gera um token a partir do env.authSecret
+    3 - o token tem data de expiracao de 1 dia, ou seja pode ficar logado até um dia no máx
 */
